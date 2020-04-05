ac068ab34d6979ad10546a02bbb7cdf394ef47e2
Bajtix <39443948+Bajtix@users.noreply.github.com>
2020-04-05 11:50:03 +0200

    Initial commit

---
+++ src/main/java/com/bajtix/onesblocks/MainClass.java
@@ -0,0 +1,60 @@
+package com.bajtix.onesblocks;
+
+import com.bajtix.onesblocks.lists.ItemList;
+import net.minecraft.dispenser.ILocation;
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.IForgeRegistryEntry;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("onesblocks2") //Previous one was never finished.
+public class MainClass
+{
+    public static MainClass instance;
+    public static final String modid  = "onesblocks2";
+    private static final Logger logger = LogManager.getLogger(modid);
+-
+    public MainClass() {
+        instance = this;
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientRegistries);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        logger.info("Setup Function registered");
+    }
+
+    private void clientRegistries(final FMLClientSetupEvent event) {
+        logger.info("Client Function registered");
+    }
+
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents
+    {
+        @SubscribeEvent
+        public static void registerItems(final RegistryEvent.Register<Item> event)
+        {
+            event.getRegistry().registerAll(
+                    ItemList.sandstone_dust.setRegistryName(location("sandstone_dust"))
+            );
+
+            logger.info("Items registered");
+        }
+
+        private static ResourceLocation location(String name)
+        {
+            return new ResourceLocation(modid,name);
+        }
+    }
+
+}
---
+++ src/main/java/com/bajtix/onesblocks/lists/ItemList.java
@@ -0,0 +1,12 @@
+package com.bajtix.onesblocks.lists;
+
+import net.minecraft.item.Food;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+
+public class ItemList
+{
+    public static Item sandstone_dust = new Item(new Item.Properties()
+            .group(ItemGroup.MISC)
+    );
+}
