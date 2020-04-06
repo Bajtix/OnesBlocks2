09400eedc4c4cfeea675fd511aed0547b16f62f2
Bajtix <39443948+Bajtix@users.noreply.github.com>
2020-04-06 15:50:35 +0200

    TextureTool.exe upgrade

---
+++ src/main/java/com/bajtix/onesblocks/AllToolGroup.java
@@ -0,0 +1,18 @@
+package com.bajtix.onesblocks;
+
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.Item;
+
+public class AllToolGroup {
+    public Item
+    pickaxe,
+    axe,
+    sword,
+    shovel,
+    hoe;
+
+    public AllToolGroup(IItemTier tier)
+    {
+
+    }
+}
