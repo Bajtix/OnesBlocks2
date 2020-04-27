cf7622809c7822b9d6acc030e1aaa15852126a89
Dragster2401 <59376519+Dragster2401@users.noreply.github.com>
2020-04-27 20:12:44 +0200

    tak o

--- src/main/java/com/bajtix/onesblocks/lists/BlockInit.java
+++ src/main/java/com/bajtix/onesblocks/lists/BlockInit.java
@@ -9,5 +9,28 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class BlockInit {
     public static final DeferredRegister<Block> register = new DeferredRegister<>(ForgeRegistries.BLOCKS, MainClass.modid);
 
+    public static final RegistryObject<Block> utility_space = register.register("utility_space", () -> BlockList.utility_space);
     public static final RegistryObject<Block> sandstone_bricks = register.register("sandstone_bricks", () -> BlockList.sandstone_bricks);
+    public static final RegistryObject<Block> sandstone_bricks_slab = register.register("sandstone_bricks_slab", () -> BlockList.sandstone_bricks_slab);
+    public static final RegistryObject<Block> sandstone_bricks_stairs = register.register("sandstone_bricks_stairs", () -> BlockList.sandstone_bricks_stairs);
+    public static final RegistryObject<Block> dunestone = register.register("dunestone", () -> BlockList.dunestone);
+    public static final RegistryObject<Block> dunestone_pillar_connector = register.register("dunestone_pillar_connector", () -> BlockList.dunestone_pillar_connector);
+    public static final RegistryObject<Block> skull_dunestone = register.register("skull_dunestone", () -> BlockList.chiseled_dunestone);
+    public static final RegistryObject<Block> dunestone_slab = register.register("dunestone_slab", () -> BlockList.dunestone_slab);
+    public static final RegistryObject<Block> dunestdunestone_pillarone_stairs = register.register("dunestone_pillar", () -> BlockList.dunestone_pillar);
+    public static final RegistryObject<Block> skull_dunestone_pillar = register.register("skull_dunestone_pillar", () -> BlockList.chiseled_dunestone_pillar);
+    public static final RegistryObject<Block> mossy_stone = register.register("mossy_stone", () -> BlockList.mossy_stone);
+    public static final RegistryObject<Block> vase = register.register("vase", () -> BlockList.vase);
+    public static final RegistryObject<Block> candle = register.register("candle", () -> BlockList.candle);
+    public static final RegistryObject<Block> big_vase = register.register("big_vase", () -> BlockList.big_vase);
+    public static final RegistryObject<Block> elfium_ore = register.register("elfium_ore", () -> BlockList.elfium_ore);
+    public static final RegistryObject<Block> mithril_ore = register.register("mithril_ore", () -> BlockList.mithril_ore);
+    public static final RegistryObject<Block> titanium_ore = register.register("titanium_ore", () -> BlockList.titanium_ore);
+    public static final RegistryObject<Block> copper_ore = register.register("copper_ore", () -> BlockList.copper_ore);
+    public static final RegistryObject<Block> tin_ore = register.register("tin_ore", () -> BlockList.tin_ore);
+    public static final RegistryObject<Block> titanium_block = register.register("titanium_block", () -> BlockList.titanium_block);
+    public static final RegistryObject<Block> mithril_block = register.register("mithril_block", () -> BlockList.mithril_block);
+    public static final RegistryObject<Block> tin_block = register.register("tin_block", () -> BlockList.tin_block);
+    public static final RegistryObject<Block> elfium_block = register.register("elfium_block", () -> BlockList.elfium_block);
+    public static final RegistryObject<Block> bronze_block = register.register("bronze_block", () -> BlockList.bronze_block);
 }
--- src/main/java/com/bajtix/onesblocks/lists/BlockList.java
+++ src/main/java/com/bajtix/onesblocks/lists/BlockList.java
@@ -15,7 +15,7 @@ public class BlockList {
             .hardnessAndResistance(2.0f, 3.0f)
             .sound(SoundType.STONE)
             .doesNotBlockMovement()
-    ).setRegistryName(MainClass.location("utility"));
+    );
 
     //Decoration
     public static Block sandstone_bricks = new Block(Block.Properties.create(Material.ROCK)
@@ -25,46 +25,46 @@ public class BlockList {
 
     public static Block sandstone_bricks_slab = new SlabBlock(
             Block.Properties.from(sandstone_bricks)
-    ).setRegistryName(MainClass.location("sandstone_bricks_slab"));
+    );
 
     public static Block sandstone_bricks_stairs = new StairsBlock(() -> sandstone_bricks.getDefaultState(),
             Block.Properties.from(sandstone_bricks)
-    ).setRegistryName(MainClass.location("sandstone_bricks_stairs"));
+    );
 
     public static Block dunestone = new Block(Block.Properties.create(Material.ROCK)
             .hardnessAndResistance(2.0f, 3.0f)
             .sound(SoundType.STONE)
-    ).setRegistryName(MainClass.location("dunestone"));
+    );
 
     public static Block dunestone_pillar_connector = new Block(
             Block.Properties.from(dunestone)
-    ).setRegistryName(MainClass.location("dunestone_pillar_connector"));
+    );
 
     public static Block chiseled_dunestone = new Block(
             Block.Properties.from(dunestone)
-    ).setRegistryName(MainClass.location("skull_dunestone"));
+    );
 
     public static Block dunestone_slab = new SlabBlock(
             Block.Properties.from(dunestone)
-    ).setRegistryName(MainClass.location("dunestone_slab"));
+    );
 
     public static Block dunestone_stairs = new StairsBlock(() -> sandstone_bricks.getDefaultState(),
             Block.Properties.from(dunestone)
-    ).setRegistryName(MainClass.location("dunestone_stairs"));
+    );
 
     public static Block dunestone_pillar = new RotatedPillarBlock(
             Block.Properties.from(dunestone)
-    ).setRegistryName(MainClass.location("dunestone_pillar"));
+    );
 
     public static Block chiseled_dunestone_pillar = new RotatedPillarBlock(
             Block.Properties.from(dunestone)
-    ).setRegistryName(MainClass.location("skull_dunestone_pillar"));
+    );
 
 
     public static Block mossy_stone = new Block(Block.Properties.create(Material.ROCK)
             .hardnessAndResistance(2.0f, 3.0f)
             .sound(SoundType.STONE)
-    ).setRegistryName(MainClass.location("mossy_stone"));
+    );
 
 
     public static Block vase = new VaseBlocks(Block.Properties.create(Material.ROCK)
@@ -78,14 +78,14 @@ public class BlockList {
             .hardnessAndResistance(2.0f, 3.0f)
             .sound(SoundType.STONE)
             .lightValue(16)
-    ).setRegistryName(MainClass.location("candle"));
+    );
 
     public static Block big_vase = new DoubleVaseBlock(Block.Properties.create(Material.ROCK)
             .hardnessAndResistance(2.0f, 3.0f)
             .sound(SoundType.STONE)
             .func_226896_b_()
             .variableOpacity()
-    ).setRegistryName(MainClass.location("big_vase"));
+    );
 
 
     //Ores
@@ -93,55 +93,55 @@ public class BlockList {
             .hardnessAndResistance(2.0f, 3.0f)
             .sound(SoundType.STONE)
             .lightValue(7)
-    ).setRegistryName(MainClass.location("elfium_ore"));
+    );
 
     public static Block mithril_ore = new Block(Block.Properties.create(Material.ROCK)
             .hardnessAndResistance(2.0f, 3.0f)
             .sound(SoundType.STONE)
-    ).setRegistryName(MainClass.location("mithril_ore"));
+    );
 
     public static Block titanium_ore = new Block(Block.Properties.create(Material.ROCK)
             .hardnessAndResistance(2.0f,3.0f)
             .sound(SoundType.STONE)
-    ).setRegistryName(MainClass.location("titanium_ore"));
+    );
 
     public static Block copper_ore = new Block(Block.Properties.create(Material.ROCK)
             .hardnessAndResistance(2.0f,3.0f)
             .sound(SoundType.STONE)
-    ).setRegistryName(MainClass.location("copper_ore"));
+    );
 
     public static Block tin_ore = new Block(Block.Properties.create(Material.ROCK)
             .hardnessAndResistance(2.0f,3.0f)
             .sound(SoundType.STONE)
-    ).setRegistryName(MainClass.location("tin_ore"));
+    );
 
 
     //Block blocks
     public static Block titanium_block = new Block(Block.Properties.create(Material.ROCK)
             .hardnessAndResistance(2.0f,3.0f)
             .sound(SoundType.STONE)
-    ).setRegistryName(MainClass.location("titanium_block"));
+    );
 
     public static Block mithril_block = new Block(Block.Properties.create(Material.ROCK)
             .hardnessAndResistance(2.0f,3.0f)
             .sound(SoundType.STONE)
-    ).setRegistryName(MainClass.location("mithril_block"));
+    );
 
     public static Block tin_block = new Block(Block.Properties.create(Material.ROCK)
             .hardnessAndResistance(2.0f, 3.0f)
             .sound(SoundType.STONE)
-    ).setRegistryName(MainClass.location("tin_block"));
+    );
 
     public static Block elfium_block = new Block(Block.Properties.create(Material.ROCK)
             .hardnessAndResistance(2.0f, 3.0f)
             .sound(SoundType.STONE)
             .lightValue(10000)
-    ).setRegistryName(MainClass.location("elfium_block"));
+    );
 
     public static Block bronze_block = new Block(Block.Properties.create(Material.ROCK)
             .hardnessAndResistance(2.0f, 3.0f)
             .sound(SoundType.STONE)
-    ).setRegistryName(MainClass.location("bronze_block"));
+    );
 
     /*public static Block[] blocks = new Block[]
             {
