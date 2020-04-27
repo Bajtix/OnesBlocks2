02871462bd4010e752b22bca6eaac4920dc33f09
Dragster2401 <59376519+Dragster2401@users.noreply.github.com>
2020-04-27 20:25:49 +0200

    tak o

--- src/main/java/com/bajtix/onesblocks/lists/ItemInit.java
+++ src/main/java/com/bajtix/onesblocks/lists/ItemInit.java
@@ -10,6 +10,11 @@ public class ItemInit {
 
     public static final DeferredRegister<Item> register = new DeferredRegister<>(ForgeRegistries.ITEMS, MainClass.modid);
 
+    public static final RegistryObject<Item> sandstone_dust = register.register("sandstone_dust", () -> ItemList.sandstone_dust);
     public static final RegistryObject<Item> sandstone_brick = register.register("sandstone_brick", () -> ItemList.sandstone_brick);
+    public static final RegistryObject<Item> tin_ingot = register.register("tin_ingot", () -> ItemList.tin_ingot);
+    public static final RegistryObject<Item> mithril_ingot = register.register("mithril_ingot", () -> ItemList.mithril_ingot);
+    public static final RegistryObject<Item> elfium = register.register("elfium", () -> ItemList.elfium);
+    public static final RegistryObject<Item> bronze_ingot = register.register("bronze_ingot", () -> ItemList.bronze_ingot);
 
 }
--- src/main/java/com/bajtix/onesblocks/lists/ItemList.java
+++ src/main/java/com/bajtix/onesblocks/lists/ItemList.java
@@ -7,7 +7,7 @@ public class ItemList {
 
     public static Item sandstone_dust = new Item(new Item.Properties()
             .group(MainClass.mainGroup)
-    ).setRegistryName(MainClass.location("sandstone_dust"));
+    );
 
     public static Item sandstone_brick = new Item(new Item.Properties()
             .group(MainClass.mainGroup)
@@ -15,19 +15,19 @@ public class ItemList {
 
     public static Item tin_ingot = new Item(new Item.Properties()
             .group(MainClass.mainGroup)
-    ).setRegistryName(MainClass.location("tin_ingot"));
+    );
 
     public static Item mithril_ingot = new Item(new Item.Properties()
             .group(MainClass.mainGroup)
-    ).setRegistryName(MainClass.location("mithril_ingot"));
+    );
 
     public static Item elfium = new Item(new Item.Properties()
             .group(MainClass.mainGroup)
-    ).setRegistryName(MainClass.location("elfium"));
+    );
 
     public static Item bronze_ingot = new Item(new Item.Properties()
             .group(MainClass.mainGroup)
-    ).setRegistryName(MainClass.location("bronze_ingot"));
+    );
 
     /*
     public static Item[] items = new Item[]
